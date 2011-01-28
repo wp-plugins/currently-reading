7b21e1d3f20b4dd53d28a107c034bae1b4538e5f
eroux <eroux@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-28 19:48:27 +0000

    Initial Release of the "Currently Reading" Wordpress Widgit
    git-svn-id: https://plugins.svn.wordpress.org/currently-reading/trunk@338302 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ CurrentlyReading.php
@@ -0,0 +1,138 @@
+<?php
+/*
+    Plugin Name: Currently Reading
+    Plugin URI: http://www.damn.org.za/blog/CurrentlyReading
+    Description: Display a Currently Reading widget using an image from (and linking to) the Google Books Website
+    Author: Eugéne Roux
+    Version: 2
+    Author URI: http://damn.org.za/
+ */
+
+//
+//  CurrentlyReading Class
+//
+class CurrentlyReading extends WP_Widget {
+    /** constructor */
+    function CurrentlyReading() {
+        $widget_ops = array( 'classname' => 'widget_reading', 'description' => __( 'Display a Currently Reading widget using an image from (and linking to) the Google Books website' ), 'internalcss' => true );
+        $this->WP_Widget( 'reading', __( 'Reading', 'reading_widget' ), $widget_ops );
+        $this->widget_defaults = array(
+            'internalcss' => true,
+        );
+    }
+
+/*
+
+    function shailan_SubpagesWidget() {
+		$widget_ops = array('classname' => 'shailan_SubpagesWidget', 'description' => __( 'Subpages list', 'subpages-extended' ) );
+		$this->WP_Widget('shailan-subpages-widget', __('Subpages Extended', 'subpages-extended'), $widget_ops);
+		$this->alt_option_name = 'widget_shailan_subpages';
+		
+		if ( is_active_widget(false, false, $this->id_base) )
+			add_action( 'wp_head', array(&$this, 'styles') );	
+
+		$this->sort_options = array(
+			'Post Title' => 'post_title',
+			'Menu Order' => 'menu_order, post_title',
+			'Date' => 'post_date',
+			'Last Modified' => 'post_modified',
+			'Page ID' => 'ID',
+			'Page Author' => 'post_author',
+			'Page Slug' => 'post_name'
+		);
+			
+		$this->widget_defaults = array(
+			'title' => '',
+			'exclude' => '',
+			'depth' => -1,
+			'use_parent_title' => false,
+			'exceptme' => false,
+			'childof' => '',
+			'sort_by' => 'menu_order, post_title',
+			'use_menu_labels' => false,
+			'link_on_title' => false
+		);
+    }
+ */
+
+    //
+    //  @see WP_Widget::widget
+    //
+    function widget($args, $instance) {		
+        $args = wp_parse_args( $args, $this->widget_defaults );
+        extract( $args );
+		//$widget_options = wp_parse_args( $instance, $this->widget_defaults );
+
+        $title = apply_filters('widget_title', $instance['title']);
+        $internalcss = $instance["internalcss"] ? true : false;
+
+        if ($instance['isbn'] != "") {      // No point in a "Currently Reading" if you aren't, is there?
+
+            echo $before_widget;
+
+            if ( $title )
+                echo $before_title . $title . $after_title; // This way we get to choose a "No Title" scenario...
+
+            $spacechars = array(' ', '-', '_');
+            $myisbn = str_replace($spacechars, "", $instance['isbn']);
+
+            print "  <!-- ISBN: " . $instance['isbn'] . " -->\n";
+            if ( $internalcss ) {
+                print "  <ul style='list-style: none'>\n";
+            } else {
+                print "  <ul>\n";
+            }
+            print"    <li>\n";
+            print "      <a href='http://books.google.com/books?vid=ISBN$myisbn'>";
+            print "<img src='http://books.google.com/books?vid=ISBN$myisbn&printsec=frontcover&img=1&zoom=1' ";
+            print "alt='ISBN: " . $instance['isbn'] . "' title='ISBN: " . $instance['isbn'] . "'/></a>\n";
+            print "    </li>\n  </ul>\n";
+
+            echo $after_widget;
+        }
+    }
+
+    //
+    //  @see WP_Widget::update
+    //
+    function update($new_instance, $old_instance) {				
+        $instance = $old_instance;
+        $instance['title'] = strip_tags( $new_instance['title'] );
+        $instance['isbn'] = strip_tags( $new_instance['isbn'] );
+        $instance['internalcss'] = $new_instance['internalcss'] ? 1 : 0;
+        return $instance;
+    }
+
+    //
+    //  @see WP_Widget::form
+    //
+    function form( $instance ) {
+        $instance = wp_parse_args( $instance, $this->widget_defaults );
+        extract( $instance );
+
+        $title = esc_attr( $instance['title'] );
+        $isbn = esc_attr( $instance['isbn'] );
+        $internalcss = $instance['internalcss'] ? "checked='checked'" : "";
+
+        print( "\t<p>\n\t\t<label for='" . $this->get_field_id("title") . "'>" ); _e( "Title:" ); 
+        print( "\n\t\t\t<input class='widefat' id='" . $this->get_field_id('title') . "' name='" );
+        print( $this->get_field_name('title') . "' type='text' value='" . $title );
+        print( "' />\n\t\t</label>\n\t\t<em>Leave blank for no title</em>\n\t</p>\n" );
+
+        print( "\t<p>\n\t\t<label for='" . $this->get_field_id("isbn") . "'>" ); _e( "ISBN:" );
+        print( "\n\t\t\t<input class='widefat' id='" . $this->get_field_id("isbn") . "' name='" );
+        print( $this->get_field_name("isbn") . "' type='text' value='" . $isbn . "' />\n\t\t</label>\n\t</p>\n" );
+
+        print( "\t<p>\n\t\t<input class='checkbox' type='checkbox' " . $internalcss );
+        print( " id='" . $this->get_field_id("internalcss") . "' name='" . $this->get_field_name("internalcss") . "'/>\n" );
+        print( "\t\t<label for='" . $this->get_field_id("internalcss") . "'>" ); _e( "Suppress List Marker" );
+        print( "</label>\n\t</p>\n" );
+
+    }
+}
+
+//
+//  register CurrentlyReading widget
+//
+add_action('widgets_init', create_function('', 'return register_widget( "CurrentlyReading" );'));
+
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Currently Reading ===
+Contributors: eroux
+Tags: books, read, reading, admin, administration, jadb
+Requires at least: 2.8
+Tested up to: 3.0.4
+Stable tag: 1.0.0
+
+Displays a cover image of a book with a link to [Google Books](http://http://books.google.com/) based on a supplied ISBN-10 or ISBN-13 (for more info, see [International Standard Book Number](http://en.wikipedia.org/wiki/International_Standard_Book_Number)).
+
+== Description ==
+Supplying an ISBN (and, optionally, a Title) will display a cover image of the relevant book with a link to that book's page on [Google Books](http://http://books.google.com/).
+
+Using the Widget you can choose whether to:
+
+1. Suppress the List Marker (selected by default, uses internal CSS)
+
+as well as
+
+1. Decide whether you would like to use a Title
+1. Define the ISBN-10 or ISBN-13 of the book.
+
+== Installation ==
+**Install**
+
+1. Unzip the `currently-reading.zip` file. 
+1. Upload `currently-reading.php` to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Use the "Currently Reading" widget.
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.0 =
+* Initial Public Release
+
